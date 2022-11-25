4acf6668f95503aa2e87cc58c6b1f96a0494e273
Fauvel Joao Murilo P <FauvelJoaoMuriloP@JohnDeere.com>
2022-11-25 14:57:24 -0300

    adding logging, related options, typing and documentation

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # climafeis
-Script CLI em Python para scrape do banco de dados climatológicos do [Canal CLIMA](http://clima.feis.unesp.br) da [UNESP Ilha Solteira](https://www.feis.unesp.br/) com a biblioteca [Requests](https://requests.readthedocs.io/en/latest/).  
+Script CLI em Python para scrape do banco de dados climatológicos do [Canal CLIMA](https://clima.feis.unesp.br) da [UNESP Ilha Solteira](https://www.feis.unesp.br/) com a biblioteca [Requests](https://requests.readthedocs.io/en/latest/).  
 
-### Instalação
+### Installation
 1. Certifique-se que o Python 3.8 ou superior e o pip estejam instalados
 1. Rode `pip install climafeis`
 
-### Instalação para desenvolvimento
+### Development
 1. Confira sua versão do Python com `python -V`
 1. Instale o Python 3.8 ou superior e pip, caso já não estejam instalados:
 
@@ -18,7 +18,31 @@ Script CLI em Python para scrape do banco de dados climatológicos do [Canal CLI
 1. Clone esse repositório com `git clone https://github.com/joaofauvel/climafeis.git && cd climafeis`
 1. Instale os requisitos `poetry install`
 
-### Utilização
+### Output headers
+| Header     | Description                                           |
+| ---------- | ----------------------------------------------------- |
+| Date       | Observation date (dd-mm-yyyy)                         |
+| Tmean      | Mean temperature (ºC)                                 |
+| Tmax       | Max temperature (ºC)                                  |
+| Tmin       | Min temperature (ºC)                                  |
+| RHmean     | Mean relative humidity (%)                            |
+| RHmax      | Max relative humidity (%)                             |
+| RHmin      | Min relative humidity (%)                             |
+| Pmean      | Mean pressure (kPa)                                   |
+| Rn         | Net radiation (MJ/m^2*day)                            |
+| Rl         | Liquid radiation (MJ/m^2*day)                         |
+| G          | Soil heat flux (MJ/m^2*day)                           |
+| PAR        | (μmoles/m^2)                                          |
+| ETcat      | Evapotranspiration Class A Tank (mm/day)              |
+| ET0pm      | Reference evapotranspiration Penman–Monteith (mm/day) |
+| ET0cat     | Reference evapotranspiration Class A Tank (mm/day)    |
+| U2max      | Max windspeed at 2 meters (m/s)                       |
+| U2mean     | Mean windspeed at 2 meters (m/s)                      |
+| U2dir      | Wind direction at 2 meters (º)                        |
+| Rain       | Rainfall (mm)                                         |
+| Insolation | Solar insolation (h/day)                              |
+
+### Usage
     usage: climafeis [-h] [-U USER] [-P PW] estacao dataInicial [dataFinal]
 
     Scrape dados diários do Canal CLIMA (http://clima.feis.unesp.br).
@@ -35,13 +59,11 @@ Script CLI em Python para scrape do banco de dados climatológicos do [Canal CLI
         -U USER, --user USER  Usuário do Canal CLIMA. Caso seu usuário não esteja em $USER_CLIMAFEIS.
         -P PW, --pw PW        Senha do Canal CLIMA. Caso sua senha não esteja em $PASSWD_CLIMAFEIS.
         
-    Exemplos:
-        climafeis ILHA_SOLTEIRA 30/05/2020 03/06/2020             Extraí dados diários da estação de Ilha
-        Solteira dos dias 30/05/2020 ao dia 03/05/2020
+    Examples:
+        climafeis ILHA_SOLTEIRA 30/05/2020 03/06/2020             Daily data from ILHA_SOLTEIRA station from 30/05/2020 to 03/05/2020
         
-        climafeis MARINOPOLIS 30/05/2020                          Extraí dados diários da estação de
-        Marinópolis dos dias 30/05/2020 ao presente dia
+        climafeis MARINOPOLIS 30/05/2020                          Daily data from MARINOPOLIS station from 30/05/2020 to today
         
-        climafeis ILHA_SOLTEIRA 30/05/2020 -U usuário -P senha    
+        climafeis ILHA_SOLTEIRA 30/05/2020 -U user -P password    
 
 
--- climafeis/climascraper.py
+++ climafeis/climascraper.py
@@ -2,6 +2,7 @@ import argparse
 import os
 from datetime import date
 from enum import Enum
+import logging
 
 import bs4
 import numpy as np
@@ -9,7 +10,7 @@ import pandas as pd
 import requests
 
 
-class Estacao(Enum):
+class Station(Enum):
     ILHA_SOLTEIRA = 1
     MARINOPOLIS = 2
     JUNQUEIROPOLIS = 3
@@ -23,77 +24,95 @@ class Estacao(Enum):
     DRACENA = 19
 
 
-def login(user, pw):
+class ParsingError(Exception):
+    pass
+
+
+def login(user: str, pw: str) -> requests.Session:
     url = 'http://clima.feis.unesp.br/valida.php'
     payload = {'usuario': user, 'senha': pw, 'enviar': 'Enviar'}
-
+    logging.debug(f'logging in at {url} with payload {payload}')
     s = requests.Session()
-    s.post(url, data=payload)
-
+    r = s.post(url, data=payload)
+    r.raise_for_status()
+    logging.debug(f'response status {r.status_code}')
     return s
 
 
 # Fmt data: dd/MM/YYYY (03/06/2020)
-def fetch_daily(session, dataini, datafim, estacao):
+def fetch_daily(session: requests.Session, dataini: str, datafim: str, estacao: Station) -> pd.DataFrame:
     url = 'http://clima.feis.unesp.br/recebe_formulario.php'
     payload = {'requireddataini': dataini, 'requireddatafim': datafim, 'estacao': estacao.value,
                'RadioGroup1': 'dados_diario', 'enviar': 'Enviar'}
-
+    logging.debug(f'POST request for fetching daily data at {url} with payload {payload}')
     r = session.post(url, payload)
+    r.raise_for_status()
+    logging.debug(f'response status {r.status_code}')
     soup = bs4.BeautifulSoup(r.content, 'html5lib')
-    table_daily = soup.find_all('table')[1]
-    try:
-        df = parse_daily(table_daily.prettify())
-    except Exception as e:
-        print(f'[ERROR] Failed to parse HTML table: {e}')
-        with open('response.html', 'wb') as f:
-            f.write(r.content)
-        return -1
-
-    df.to_csv('planilha.csv')
-    return 0
-
-
-def parse_daily(daily_data):
-    headers = {0: 'Tmedia', 1: 'Tmax', 2: 'Tmin',
-               3: 'Urel_media', 4: 'Urel_max', 5: 'Urel_min',
-               6: 'P_atm', 7: 'R_global', 8: 'R_liq', 9: 'Flux_calor',
-               10: 'PAR', 11: 'Ev_tca', 12: 'ET0_pm', 13: 'ET0_tca', 14: 'Vvento_max',
-               15: 'Vvento_media', 16: 'Dir_vento', 17: 'Chuva', 18: 'Insolacao'}
-    headers = pd.Series(headers)
-
-    dfs = pd.read_html(daily_data, index_col=0, parse_dates=True, flavor='html5lib')
-
-    df = pd.concat(dfs, axis=1, sort=False, ignore_index=True)
-    df = df[2:-11]
-    df = df[[i for i in range(19)]]
-
-    df = df.rename(columns=headers)
-    df.index.name = 'Data'
-    df = df.replace('-', np.nan)
-
+    table_daily: bs4.element.Tag = soup.find_all('table')[1]
+    df = parse_daily(table_daily.prettify())
     return df
 
 
-def main():
-    parser = argparse.ArgumentParser(description='Scrape dados diários do Canal CLIMA (http://clima.feis.unesp.br).')
-    parser.add_argument('estacao', type=str,
-                        help="""Nome da estação: ILHA_SOLTEIRA, MARINOPOLIS, JUNQUEIROPOLIS, PARANAPUA, IRAPURU, 
+def parse_daily(daily_data: str) -> pd.DataFrame:
+    headers = {0: 'Tmean', 1: 'Tmax', 2: 'Tmin',
+               3: 'RHmean', 4: 'RHmax', 5: 'RHmin',
+               6: 'Pmean', 7: 'Rn', 8: 'Rl', 9: 'G',
+               10: 'PAR', 11: 'ETcat', 12: 'ET0pm', 13: 'ET0cat', 14: 'U2max',
+               15: 'U2mean', 16: 'U2dir', 17: 'Rain', 18: 'Insolation'}
+    
+    logging.debug('reading table from html table daily_data')
+    dfs = pd.read_html(daily_data, index_col=0, parse_dates=True, flavor='html5lib')
+    try:
+        # due to the html table structure in this page, it is necessary to concatenate 
+        # along the columns to create a single table 
+        df = pd.concat(dfs, axis=1, sort=False, ignore_index=True)
+    except pd.errors.InvalidIndexError:
+        logging.error('unable to parse response, credentials might be wrong')
+        raise ParsingError('unable to parse response, check your login credentials')
+    # trimming html table 'junk'
+    df = df[2:-11]
+    # filtering target column indices
+    df = df[[i for i in range(19)]]
+    df = df.rename(columns=pd.Series(headers))
+    df.index.name = 'Date'
+    df = df.replace('-', np.nan)
+    return df
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description='Scrape daily climate date from Canal CLIMA (https://clima.feis.unesp.br).')
+    parser.add_argument('station', type=str,
+                        help="""Station name: ILHA_SOLTEIRA, MARINOPOLIS, JUNQUEIROPOLIS, PARANAPUA, IRAPURU, 
                         POPULINA, SANTA_ADELIA_PIONEIROS, SANTA_ADELIA, BONANCA, ITAPURA, DRACENA.""")
-    parser.add_argument('dataInicial', type=str, help='Data inicial no formato dd/MM/YYYY (30/05/2020).')
-    parser.add_argument('dataFinal', nargs='?', default=date.today().strftime('%d/%m/%Y'), type=str,
-                        help='Data final no formato dd/MM/YYYY (03/05/2020). Padrão: presente dia.')
+    parser.add_argument('start', type=str, help='Start date dd/MM/YYYY (30/05/2020).')
+    parser.add_argument('end', nargs='?', default=date.today().strftime('%d/%m/%Y'), type=str,
+                        help='End date dd/MM/YYYY (03/05/2020). Default: today.')
     parser.add_argument('-U', '--user', type=str,
-                        help='Usuário do Canal CLIMA. Caso seu usuário não esteja em $USER_CLIMAFEIS.')
+                        help='Override Canal CLIMA user set in the environment variable $USER_CLIMAFEIS.')
     parser.add_argument('-P', '--pw', type=str,
-                        help='Senha do Canal CLIMA. Caso sua senha não esteja em $PASSWD_CLIMAFEIS.')
+                        help='Override Canal CLIMA password set in the environment variable $PASSWD_CLIMAFEIS.')
+    parser.add_argument('-o', '--output', metavar='OUT', type=str,
+                        help='Output file. Default: <station>.csv')
+    parser.add_argument('-l', '--log', type=str, help='Output log file. Default: stdout')
+    parser.add_argument('-v', '--verbose', action='count', default=1)
     args = parser.parse_args()
+    args.verbose = 40 - (10*args.verbose) if args.verbose > 0 else 0
+    FORMAT = '%(asctime)s %(levelname)s: %(message)s'
+    DATEFMT = '%Y-%m-%d %H:%M:%S'
+    logging.basicConfig(level=args.verbose, format=FORMAT, datefmt=DATEFMT, filename=args.log)
+
     if args.user or args.pw:
+        logging.info('attempting login with overriden user and password parameters')
         s = login(args.user, args.pw)
     else:
+        logging.info('attempting login with credentials stored in environment variables')
         s = login(os.environ['USER_CLIMAFEIS'], os.environ['PASSWD_CLIMAFEIS'])
-
-    fetch_daily(s, args.dataInicial, args.dataFinal, Estacao[args.estacao])
+    logging.info('fetching daily data')
+    df = fetch_daily(s, args.start, args.end, Station[args.station])
+    outputf = f'{args.output}.csv' if args.output else f'{args.station}.csv'
+    logging.info(f'writing to {outputf}')
+    df.to_csv(outputf)
 
 
 if __name__ == '__main__':
